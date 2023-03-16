d363d762dd09843cf17a0f4cb74ad16be7d2d5ed
Emerlyy <vadim200339@gmail.com>
2023-03-17 00:12:25 +0200

    changed vh to svh

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import MultiStepForm from "./components/MultiStepForm/MultiStepForm";
 
 function App() {
   return (
-    <div className="relative min-h-screen min-w-screen sm:grid sm:place-items-center bg-[#eef5ff]">
+    <div className="relative min-h-[100svh] min-w-screen sm:grid sm:place-items-center bg-[#eef5ff]">
         <div className="container py-12 sm:py-0 px-5 lg:px-20">
         <MultiStepForm />
         </div>
